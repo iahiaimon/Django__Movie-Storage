6644b6ab21ea1f7f3751a56e818926b191c85d67
iahia <iahiaimon555@gmail.com>
2025-04-22 02:14:19 +0600

    Uploading

--- home/urls.py
+++ home/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
 
     path('' , views.home , name= "home"),
     path('register/' , views.register , name = "register"),
+    path("login/" , views.login , name="login"),
     path('addmovie/' , views.addmovie , name = "addmovie"),
 
 ]
--- home/views.py
+++ home/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse, JsonResponse
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
-from .forms import CustomUserForm , AddMovieForm
+from .forms import CustomUserForm, AddMovieForm
+from django.contrib import messages
 
 # Create your views here.
 
@@ -12,27 +13,43 @@ def home(request):
 
 
 def register(request):
-    if request.method == 'POST':
+    if request.method == "POST":
         form = CustomUserForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('home')
+            return redirect("home")
 
     else:
         form = CustomUserForm()
 
-    return render(request , 'register.html' , {'form': form} )
+    return render(request, "register.html", {"form": form})
 
+
+def login(request):
+    if request.method == "POST":
+        email = request.POST.get("email")
+        password = request.POST.get("password")
+        user = authenticate(request, email=email, password=password)
+        if not user:
+            messages.error(request, "Invalid username or password.")
+        elif not user.is_verified:
+            messages.error(request, "Your email is not verified yet.")
+        else:
+            login(request, user)
+            messages.success(request, "You have successfully logged in.")
+            return redirect("home")
+
+    return render(request, "login.html")
 
 
 def addmovie(request):
-    if request.method == 'POST':
-        form = AddMovieForm(request.POST , request.FILES)
+    if request.method == "POST":
+        form = AddMovieForm(request.POST, request.FILES)
         if form.is_valid():
             form.save()
-            return redirect('home')
-    
+            return redirect("home")
+
     else:
         form = AddMovieForm()
-    
-    return render(request , 'addmovie.html' , {'form' : form})
+
+    return render(request, "addmovie.html", {"form": form})
